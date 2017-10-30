87b4262027b925f688ebbc29d7ae121191e44992
Harley <harleymount@hotmail.com>
2017-10-30 11:28:04 -0400

    Add files via upload

--- Analyze_reads.sh
+++ Analyze_reads.sh
@@ -6,8 +6,8 @@ dir=$1;
 file=$2
 cd $dir;
 
-python analyze_reads.py $file
+python analyze_reads.py $file;
 
 
-Rscript plot_crossovers.R discontinuous_hist.csv
-Rscript plot_losses.R continuous_hist.csv
+Rscript plot_crossovers.R $dir discontinuous_hist.csv;
+Rscript plot_losses.R $dir continuous_hist.csv;
