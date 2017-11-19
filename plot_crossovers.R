a6d5eee5f2e05ed24e067cc553c39dd63e0a3371
Harley <harleymount@hotmail.com>
2017-11-19 12:59:38 -0500

    updated, more flexbile, nicer plots

--- plot_crossovers.R
+++ plot_crossovers.R
@@ -1,5 +1,7 @@
 args = commandArgs(trailingOnly=TRUE)
 hist_file=args[2]
+hist_file='discontinuous_hist.csv'
+dir='/Users/Harley/Desktop/test_fast5/'
 dir=args[1]
 hist_data<-read.csv(hist_file)
 setwd(dir)
@@ -10,10 +12,13 @@ Cleanup<- theme(panel.grid.major=element_blank(),#remove teh major gridlines
                 axis.line=element_line(color="black"))#make the axis line black
 
 
+length(hist_data$read_name)
+
+table(hist_data)
 
 tiff("crossover_frequency.tiff", height = 12, width = 12, units = 'cm', 
      compression = "lzw", res = 300)
-ggplot(hist_data, aes(x=crossovers))+geom_histogram(binwidth=1,color='dark blue',fill='gray70')+Cleanup+labs(x='Number of Crossovers', y='Frequency')+ggtitle('Crossover Frequency Among Reads') + theme(plot.title = element_text(hjust = 0.5)) + scale_x_continuous(breaks=seq(0,5,1), limits=c(0,5))+scale_y_continuous(breaks=seq(0,100,2), limits=c(0,30))
+ggplot(hist_data, aes(x=crossovers))+geom_histogram(binwidth=1,color='white',fill='gray0')+Cleanup+labs(x='Number of Crossovers', y='Frequency')+ggtitle('Crossover Frequency Among Reads') + theme(plot.title = element_text(hjust = 0.5)) + scale_x_continuous(breaks=seq(0,6,1), limits=c(0,6))
 dev.off()
 
 
