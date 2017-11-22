da4acd59b072385b5f8605af17dd460ee7d17b30
Harley <harleymount@hotmail.com>
2017-11-22 15:39:20 -0500

    fix no 0 bin
    wasnt plotting 0, fixed by removing limit in ggplot2

--- plot_crossovers.R
+++ plot_crossovers.R
@@ -12,7 +12,7 @@ Cleanup<- theme(panel.grid.major=element_blank(),#remove teh major gridlines
 
 tiff("crossover_frequency.tiff", height = 12, width = 12, units = 'cm', 
      compression = "lzw", res = 300)
-ggplot(hist_data, aes(x=crossovers))+geom_histogram(binwidth=1,color='white',fill='gray0')+Cleanup+labs(x='Number of Crossovers', y='Frequency')+ggtitle('Crossover Frequency Among Reads') + theme(plot.title = element_text(hjust = 0.5)) + scale_x_continuous(breaks=seq(0,6,1), limits=c(0,6))
+ggplot(hist_data, aes(x=crossovers))+geom_histogram(binwidth=1,color='white',fill='gray0')+Cleanup+labs(x='Number of Crossovers', y='Frequency')+ggtitle('Crossover Frequency Among Reads') + theme(plot.title = element_text(hjust = 0.5)) + scale_x_continuous(breaks=seq(0,6,1))
 dev.off()
 
 
